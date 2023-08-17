af3e2e4b46cc093030c3486c4b33f4bb24c1c480
angeloyana-dev <angeloyana.dev@gmail.com>
2023-08-17 10:46:33 +0800

    Added function to loop through font folders

--- bin/index.js
+++ bin/index.js
@@ -11,5 +11,48 @@ folderArray.pop()
 // Tracks folder while looping through folders
 let index = 0
 
-// Temporary
-console.log(mainDir, folderArray)
+openFolder()
+// Function to loop through each fonts folder
+async function openFolder(){
+	const destination = folderArray[index]
+	await cd(destination)
+	const { stdout: content } = await exec("ls")
+	// Checks if theres a static folder inside font folder
+	if(!content.includes("static")){
+		const fontFiles = content.split("\n")
+		fontFiles.pop()
+		
+		await applyFontToCss(fontFiles, false, destination.split("_"))
+		index++
+		
+		// Checks if theres still a folder to open
+		if(folderArray.length !== index){
+			await cd(mainDir)
+			
+			// Call this function again for the next folder
+			openFolder()
+		}
+	} else {
+		await cd("static")
+		const { stdout: staticContents } = await exec("ls", false)
+		let fontFiles = staticContents.split("\n")
+		fontFiles.pop()
+		
+		await applyFontToCss(fontFiles, true, destination.split("_"))
+		index++
+		
+		// Checks if theres still a folder to open
+		if(folderArray.length !== index){
+			await cd(mainDir)
+			
+			// Call this function again for the next folder
+			openFolder()
+		}
+	}
+}
+
+// Goes back to the main directory to create fonts.css later
+async function applyFontToCss(fontFiles, isStatic, fontName){
+	// Temporary
+	console.log({ fontFiles, isStatic, fontName})
+}
