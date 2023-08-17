0721d3275692d45c527da53dabf72d3bdcbae84b
angeloyana-dev <angeloyana.dev@gmail.com>
2023-08-17 09:52:22 +0800

    Created shell-exec.js module

---
+++ modules/shell-exec.js
@@ -0,0 +1,25 @@
+import shell from "shelljs"
+
+// General execution
+export async function exec(command, isNotSilent){
+	return new Promise(resolve => {
+		if(isNotSilent === true){
+			shell.exec(command, (code, stdout, stderr) => {
+				resolve({ code, stdout, stderr })
+			})
+		} else {
+			shell.exec(command, { silent: true }, (code, stdout, stderr) => {
+				resolve({ code, stdout, stderr })
+			})
+		}
+	})
+}
+
+// For change directory
+export async function cd(destination){
+	return await new Promise(resolve => {
+		shell.cd(destination, (code, stdout, stderr) => {
+			resolve({ code, stdout, stderr })
+		})
+	})
+}
